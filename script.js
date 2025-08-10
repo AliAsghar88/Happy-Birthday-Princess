caeba5ed068208bb03217b295f2b08b8101777c1
AliAsghar88 <75814953+AliAsghar88@users.noreply.github.com>
2025-08-10 09:34:21 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,32 +8,3 @@ setTimeout(() => {
 setTimeout(() => {
     celebrationDiv.style.display = "none";
 }, 12000);
-
-
-
-
-
-```
-7) your Smile melts me everytime
-9) when you wear bangles
-10) when you put henna on your hands
-11) when you make different hairstyles
-12) the way you style your dresses
-13) What You Inspire In Me
-14) What I Look Forward to: "Plans or dreams you want to share"
-15) How You Make Me Feel
-16) A Song That Reminds Me of You
-17) How You’ve Changed My Life 
-18) A Promise to You
-19) What Love Means to Me Because of You
-20) Your Best Qualities as a Partner
-21) Something I’ve Learned from You
-22) What I Miss When We’re Apart
-23) How You Make Ordinary Days Extraordinary
-24) Your Kindness and How It Inspires Me
-25) The Little Things You Do That I Notice
-26) How You Make Me a Better Person, How You Brighten My Mood on Tough Days
-27) What I Learned From Falling in Love With You
-28) The Best Compliment You Ever Gave Me
-29) Thank You for Being You
-```
